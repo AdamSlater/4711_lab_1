0215208b104fb5044ec5782782d2afe124a522d8
Adam Slater <adam.slater.1010@gmail.com>
2016-01-06 12:47:40 -0800

    implemented game class & gui

--- index.php
+++ index.php
@@ -1,37 +1,74 @@
 <?php
-function winner($token,$position) {
-    for($row=0; $row<3; $row++) { //horizontals
-        for($col=0; $col<3; $col++) {
-            if ($position[3*$row+$col] != $token)
-                break;
-            if ($col == 2) return true;
-        }
+class Game {
+    var $position;
+    var $newposition;
+
+    function __construct($squares)
+    {
+        $this->position = str_split($squares);
     }
-    for($col=0; $col<3; $col++) { //verticals
+
+    function winner($token) {
+        //horizontals
         for($row=0; $row<3; $row++) {
-            if ($position[3*$col+$row] != $token)
-                break;
-            if ($row == 2) return true;
+            for($col=0; $col<3; $col++) {
+                if ($this->position[3*$row+$col] != $token)
+                    break;
+                if ($col == 2) return true;
+            }
         }
+        //verticals
+        for($col=0; $col<3; $col++) {
+            for($row=0; $row<3; $row++) {
+                if ($this->position[3*$row+$col] != $token)
+                    break;
+                if ($row == 2) return true;
+            }
+        }
+        //diagonals
+        if
+        (
+            (
+                ($this->position[0] == $token) &&
+                ($this->position[4] == $token) &&
+                ($this->position[8] == $token)
+            )
+            ||
+            (
+                ($this->position[2] == $token) &&
+                ($this->position[4] == $token) &&
+                ($this->position[6] == $token)
+            )
+        )
+            return true;
+        return false;
     }
-    //diagonals
-    if
-    (
-        (
-            ($position[0] == $token) &&
-            ($position[4] == $token) &&
-            ($position[8] == $token)
-        )
-        ||
-        (
-            ($position[2] == $token) &&
-            ($position[4] == $token) &&
-            ($position[6] == $token)
-        )
-    )
-        return true;
-    return false;
+
+    function display() {
+        echo '<table cols="3" style="font-size:large; font-weight:bold">';
+        echo '<tr>'; // open the first row
+        for ($pos=0; $pos<9;$pos++) {
+            echo $this->show_cell($pos);
+            if ($pos %3 == 2) echo '</tr><tr>'; // start a new row for the next square
+        }
+        echo '</tr>'; // close the last row
+        echo '</table>';
+    }
+
+    function show_cell($which) {
+        $token = $this->position[$which];
+        // deal with the easy case
+        if ($token <> '­') return '<td>'.$token.'</td>';
+        // now the hard case
+        $this->newposition = $this->position; // copy the original
+        $this->newposition[$which] = 'o'; // this would be their move
+        $move = implode($this->newposition); // make a string from the board array
+            $link = '/?board='.$move; // this is what we want the link to be
+        // so return a cell containing an anchor and showing a hyphen
+        return '<td><a href="'.$link.'">-</a></td>';
+    }
 }
+
 ?>
 <html>
     <head>
@@ -39,16 +76,18 @@ function winner($token,$position) {
     </head>
     <body>
     <?php
-    $position = $_GET['board'];
-    $squares = str_split($position);
-
     if (isset($_GET['board']))
     {
-        if (winner('x', $squares))
-            echo 'You win, x';
-        else if (winner('o', $squares))
-            echo 'I win, o';
-        else echo 'No winner yet.';
+        $game = new Game($_GET['board']);
+
+        $game->display();
+
+        if ($game->winner('x'))
+            echo 'You win. Lucky guesses!';
+        else if ($game->winner('o'))
+            echo 'I win. Muahahahaha';
+        else
+            echo 'No winner yet, but you are losing.';
     }
     ?>
     </body>
