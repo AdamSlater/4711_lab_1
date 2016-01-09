e35eee81b7cc38ecbd88147fd7b31b0022a59f55
AdamSlater <Adam.Slater.1010@gmail.com>
2016-01-09 11:17:42 -0800

    added comments

--- game.php
+++ game.php
@@ -3,12 +3,24 @@ class Game {
     var $position;
     var $newposition;
 
+    /*
+     * Creates game object.
+     * Creates array of characters as a game board.
+     */
     function __construct($squares = '---------')
     {
         $this->position = str_split($squares);
     }
 
-    function winner($token) {
+    /*
+     * Returns the winner, if one exists.
+     *
+     * Goes through array
+     *          checks the next adjacent square
+     *              and only if ( it's the last square in the row )
+     */
+    function winner($token)
+    {
         //horizontals
         for($row=0; $row<3; $row++) {
             for($col=0; $col<3; $col++) {
@@ -44,7 +56,8 @@ class Game {
         return false;
     }
 
-    function display() {
+    function display()
+    {
         echo '<div class="container"><table class="table table-bordered table-responsive text-center">';
         echo '<tr>'; // open the first row
         for ($pos=0; $pos<9;$pos++) {
@@ -57,7 +70,7 @@ class Game {
 
     function show_cell($which) {
         $token = $this->position[$which];
-        $c = ($token == 'x') ? 'rgba(255,0,0,0.4)' : 'rgba(0,255,0,0.4)';
+        $c = ($token == 'x') ? 'rgba(255,0,0,0.4)' : 'rgba(0,255,0,0.4)'; // bg color for x | o
         // deal with the easy case
         if ($token <> '-') return "<td style='background-color:$c'>".$token.'</td>';
         // now the hard case
@@ -69,9 +82,11 @@ class Game {
         return '<td><a href="'.$link.'">-</a></td>';
     }
 
-    function pick_move(){
-        $options = array();
-        $x = $o = 0;
+    function pick_move()
+    {
+        $options = array(); // empty array of choices
+        $x = $o = 0;        // counter for x's and o's
+        //go through all squares
         for($row=0; $row<3; $row++)
             for($col=0; $col<3; $col++)
             {
@@ -80,17 +95,20 @@ class Game {
                 else if ($this->position[3*$row+$col] == 'o')
                     $o++;
                 else
-                    array_push($options, 3*$row+$col);
+                    array_push($options, 3*$row+$col); // adds empty square ('-') to options
             }
 
+        // there is no where for x to go, and o did not win last round
         if (empty($options))
             echo "<i>The game ended in a tie, please press replay.</i>";
 
+        // in-case of page refresh, x does not go again
         if ( $x <= $o )
         {
-            $choice = rand(1, sizeof($options));
-            $this->position[$options[$choice-1]] = 'x';
+            $choice = rand(1, sizeof($options));        // random number from 0 to range of options, offset 1
+            $this->position[$options[$choice-1]] = 'x'; // places x at random selection
 
+            // if that was the last possible move, checks if it caused ai to win
             if (sizeof($options) == 1)
                 if (!$this->winner('x'))
                     die("The game was tie :(<br><hr><a class=\"btn btn-default\" role=\"button\" onclick=\"window.location.assign('index.php')\">Replay</a>");
